def7035c23873d280cc4f93483a3638fbe56f87a
Fabio Davi <fdr.desenvolvedor@gmail.com>
2020-07-17 19:23:42 -0300

    sd

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ mongoose.connect('mongodb+srv://server:123@cluster0-bkguh.mongodb.net/bd?retryWr
 
 //quando for fazer chamadas a api externas .. .instalr o npm add cors
 //app.use(cors({ origin: 'http://localhost:3000'}))
-app.use(cors()) // a esquerda libera acesso para todas as aplicaçõpes
-
+app.use(cors()) /// a esquerda libera acesso para todas as aplicaçõpes
+ 
 app.use(express.json())
 // fazendo com que o express entenda requisições com corpo json
 // precisa vir antes das rotas ( o node lê de forma linear... de cima pra baixo)
